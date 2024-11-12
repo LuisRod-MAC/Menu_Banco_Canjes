5b0c3b97367b0655699f390659fc38d24561af4a
LuisRod-MAC <321272081@pcpuma.acatlan.unam.mx>
2024-11-12 07:34:03 -0600

    Add files via upload

---
+++ EjemplDeString(3).c
@@ -0,0 +1,40 @@
+int main(){
+	//Funcion isalnum
+	char a='1';
+	printf("El resultado de la funcion isalnum es de %d \n\n",isalnum(a));
+	
+	//Funcion isalpha
+	char b='N';
+	printf("El resultado de la funcion isalpha es de %d \n\n",isalpha(b));
+	
+	//Funcion isblank
+	char c='  ';
+	printf("El resultado de la funcion isblank es de %d \n\n",isblank(c));
+	
+	//Funcion isdigit
+	char d='7';
+	printf("El resultado de la funcion isdigit es de %d \n\n",isdigit(d));
+	
+	//Funcion isspace
+	char e=' ';
+	printf("El resultado de la funcion isspace es de %d \n\n",isspace(e));
+	
+	//Funcion islower
+	char f='a';
+	printf("El resultado de la funcion islower es de %d \n\n",islower(f));
+	
+	//Funcion isupper
+	char g='A';
+	printf("El resultado de la funcion isupper es de %d \n\n",isupper(g));
+	
+	//Funcion toupper
+	char h='n';
+	printf("El resultado de la funcion toupper es de %d \n\n",toupper(h));
+	
+	//Funcion tolower
+	char i='D';
+	printf("El resultado de la funcion tolower es de %d \n\n",tolower(i));
+	
+	system ("pause");
+	return 0;
+}
